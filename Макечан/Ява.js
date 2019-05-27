9bff7ec9fb134576ff1fd11a4e9dce9e819173e3
Myzz1k <51045233+Myzz1k@users.noreply.github.com>
2019-05-27 19:58:10 +0700

    5555

---
+++ Макечан/Ява.js
@@ -0,0 +1,34 @@
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
